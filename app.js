e4ee47f71631a6567db0a64a334b96eaaed448b0
Deniz <denizerdem@livewallgroup.com>
2023-04-30 14:35:02 +0200

    ja

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-import http from 'http';
-const app = express();
+const http = require('http');
+var app = express();
 const server = http.createServer(app);
-
-import { Server } from "socket.io";
+const { Server } = require('socket.io');
 
 const io = new Server(server, {
   cors: {
@@ -23,3 +22,5 @@ io.on('connection', (socket) => {
 server.listen(3001, () => {
   console.log('listening on *:3001');
 })
+
+module.exports = app;
